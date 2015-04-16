b43fb4246fea8e42641f4a57cbe95830e7d246ad
Mathieu Fuchs <mathieufuchs13@gmail.com>
2015-04-16 18:21:44 +0200

    nya features

--- mathieu/script/bab.js
+++ mathieu/script/bab.js
@@ -3,9 +3,24 @@ if (BABYLON.Engine.isSupported()) {
             var canvas = document.getElementById("canvas");
             var engine = new BABYLON.Engine(canvas, true);
 
-            var initBox = function(){
-
+            var createBox = function(name,s,scene,xScal,zScal,yPos){
+                var box = BABYLON.Mesh.CreateBox(name, s, scene);
+                box.position.x=0;
+                box.position.y=yPos;
+                box.scaling.x=xScal;
+                box.scaling.y=0.1;
+                box.scaling.z=zScal;
+                var material = new BABYLON.StandardMaterial("mat", scene);
+            box.material = material;
+            material.diffuseColor = new BABYLON.Color3(1.5, 0, 0);
+                return box;
             }
+            var createHouse = function(numPlan,scene){
+                for(i=0;i<numPlan;i++){
+                    scene.meshes.push(createBox("Plan"+1,10,scene,10,3,i*10));
+                }
+            }
+
             
             var createScene = function () {
     var scene = new BABYLON.Scene(engine);
@@ -16,48 +31,15 @@ if (BABYLON.Engine.isSupported()) {
 
     var light = new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(0, 1, 0), scene);
 
-    //Creation of 3 boxes and 2 spheres
-    var box1 = BABYLON.Mesh.CreateBox("Box1", 12.0, scene);
-    /*var box2 = BABYLON.Mesh.CreateBox("Box2", 6.0, scene);
-    var box3 = BABYLON.Mesh.CreateBox("Box3", 6.0, scene);
-    var box4 = BABYLON.Mesh.CreateBox("Box4", 6.0, scene);
-    var box5 = BABYLON.Mesh.CreateBox("Box5", 6.0, scene);
-    var box6 = BABYLON.Mesh.CreateBox("Box6", 6.0, scene);
-    var box7 = BABYLON.Mesh.CreateBox("Box7", 6.0, scene);*/
+    var ground = BABYLON.Mesh.CreateGround("ground", 1000, 1000, 1, scene);
+    ground.material = new BABYLON.StandardMaterial("ground", scene);
+    ground.material.diffuseColor = BABYLON.Color3.FromInts(193, 181, 151);
+    ground.material.specularColor = BABYLON.Color3.Black();
 
-    //Moving boxes on the x axis
-    box1.position.x = 0;
-    /*box2.position.x = -10;
-    box3.position.x = 0;
-    box4.position.x = 15;
-    box5.position.x = 30;
-    box6.position.x = 45;*/
+    //Creation of 3 boxes and 2 
 
-    //Rotate box around the x axis
-    box1.scaling.x=10;
-    box1.scaling.z=3;
-    box1.scaling.y=0.1;
-    box1.rotation.x = Math.PI / 6;
-
-    var box2 = box1;
-    box2.position.y=1;
-
-
-    //Rotate box around the y axis
-    /*box2.rotation.y = Math.PI / 3;
-
-    //Scaling on the x axis
-    box4.scaling.x = 2;
-
-    //Scaling on the y axis
-    box5.scaling.y = 2;
-
-    //Scaling on the z axis
-    box6.scaling.z = 2;
-
-    //Moving box7 relatively to box1
-    box7.parent = box1;
-    box7.position.z = -10;*/
+    createHouse(5,scene);
+    
 
     return scene;
 }
